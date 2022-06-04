d9d7dfef1eb18c6b9a530fbd4f1c0eae8b2fcbae
Stephania Alexis <101305146+Babygirl110609@users.noreply.github.com>
2022-06-04 14:43:49 -0500

    initial main

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const { prompt } = require("inquirer");
 const logo = require("asciiart-logo");
-const db = require("./db");
-require("console.table");
 
-init();
+require("console.table");
+const db = require("./db/connection");
+
+db.connect(function(){
+  console.log("Employee tracker");
+  init();
+})
+
+
 
 // Display logo text, load main prompts
 function init() {
@@ -17,3 +23,42 @@ function init() {
 function loadMainPrompts() {
   prompt([
     {
+    type: "list",
+    name: "choice",
+    message:"What would you like to see?",
+    choices:["View all Employee","View all Department", "View all role", "employee update", "add employes", "add a role","Exit App"]
+    }
+
+  ]).then(response => {
+    switch(response.choice){
+      case "View all Employee":
+        viewAllEmployee();
+        break;
+      
+        case "View all Department":
+        viewAllDepartment();
+        break;
+
+        case "View all role":
+        viewAllrole();
+        break;
+
+        case "employee update":
+        employeeupdate();
+        break;
+
+        case "add employee":
+          addemployee();
+          break; 
+
+          case "add a role":
+            addarole();
+            break;  
+            default:
+              db.end()
+              process.exit(0)
+    }
+  })
+
+
+  }
